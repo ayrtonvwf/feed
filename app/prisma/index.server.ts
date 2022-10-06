e0403102576f37cd5d2d844fe5768aed7be5f568
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-10-06 20:56:07 -0300

    feat: make it work
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

---
+++ app/prisma/index.server.ts
@@ -0,0 +1,42 @@
+/**
+ * @see https://github.com/marcomafessolli/remix-prisma-cloudflare-workers/blob/main/app/db.server.ts
+ */
+
+import { PrismaClient } from "@prisma/client/edge";
+
+const DATABASE_URL = 'prisma://aws-us-east-1.prisma-data.com/?api_key=AJunWDUXCpIwj3Q-Y2_MSmVEB7ubWdSIo9PK-aXRoF3lhSG82F606EoZ_NFmUrSt';
+
+let prisma: PrismaClient;
+
+declare global {
+  var __db__: PrismaClient;
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+// in production we'll have a single connection to the DB.
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({ 
+  datasources: {
+    db: {
+      // url: env.DATABASE_URL
+      url: DATABASE_URL
+    }
+  } });
+} else {
+  if (!global.__db__) {
+    global.__db__ = new PrismaClient({ 
+      datasources: {
+        db: {
+          // url: env.DATABASE_URL,
+          url: DATABASE_URL
+        }
+      }
+    });
+  }
+  prisma = global.__db__;
+  prisma.$connect();
+}
+
+export { prisma };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,39 +1,27 @@
+import { Feed } from "@prisma/client";
+import { DataFunctionArgs, json, LoaderFunction } from "@remix-run/cloudflare";
+import { Link, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/prisma/index.server";
+
+export const loader: LoaderFunction = async ({
+  request,
+  context,
+}: DataFunctionArgs) => {
+  await prisma.$connect();
+  const feeds = await prisma.feed.findMany();
+  await prisma.$disconnect();
+  return json(feeds);
+};
+
 export default function Index() {
+  const feeds = useLoaderData<Feed[]>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>SEJA MUITO BEM VINDO/A AO MEU MAIS NOVO WEBSITE</h1>
-      <h2>Ihaaaaa o Remix é dahora</h2>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://ayrtonvwf.dev"
-          >
-            Meu website pessoal
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://github.com/ayrtonvwf"
-          >
-            Meu GitHub
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://twitter.com/ayrtonvwf"
-          >
-            Meu Twitter
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run">
-            Remix
-          </a>
-        </li>
-      </ul>
+      <h1>Feeds</h1>
+      {feeds.map(feed => <>
+        <Link to={`/feed/${feed.id}`}>{feed.title}</Link>
+      </>)}
       <iframe src="https://giphy.com/gifs/season-12-the-simpsons-12x6-3orif6SZHGRvNulw0E/fullscreen" width="100%" height="600px" style={{ overflow: 'hidden' }}/>
     </div>
   );
