5ffbadd5ce0273c00e9e5b5088ff29f7d4ad66f1
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-11-06 09:06:27 -0300

    feat: adjust standalone-comment component styling #24
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

--- app/components/feed/standalone-comment.tsx
+++ app/components/feed/standalone-comment.tsx
@@ -1,6 +1,8 @@
 import { Comment, Feed, Post, User } from "@prisma/client";
 import { Panel } from "../block/panel";
+import { DateTime } from "../typography/date-time";
 import { MyLink } from "../typography/link";
+import { StandalonePost } from "./standalone-post";
 
 export type StandaloneCommentType = Comment & {
   User: User;
@@ -12,31 +14,23 @@ const descriptionLimit = 100;
 export const StandaloneComment: React.FC<{
   comment: StandaloneCommentType;
 }> = ({ comment }) => (
-  <Panel key={comment.id}>
-    <div className="mb-5 flex flex-col gap-2">
+  <Panel key={comment.id} className="border-[1px] border-solid border-gray-300">
+    <div className="flex gap-2">
       <div>
-        <MyLink to={`/user/${comment.User.id}`}>{comment.User.name}</MyLink>
-        <span>{comment.createdAt.toLocaleString()}</span>
-        <MyLink to={`/post/${comment.Post.id}?comment=${comment.id}`}>
-          Referência
-        </MyLink>
+        <MyLink to={`/user/${comment.User.id}`}>{comment.User.name}</MyLink>{" "}
+        comentou
       </div>
-      <p>
-        {comment.description.substring(0, descriptionLimit) +
-          (comment.description.length > descriptionLimit ? "…" : "")}
-      </p>
-      <div>
-        <MyLink to={`/post/${comment.Post.id}`}>{comment.Post.title}</MyLink>
-        (from{" "}
-        <MyLink to={`/user/${comment.Post.User.id}`}>
-          {comment.Post.User.name}
-        </MyLink>
-        ) (on{" "}
-        <MyLink to={`/feed/${comment.Post.Feed.id}`}>
-          {comment.Post.Feed.title}
-        </MyLink>
-        )
-      </div>
+      <div>|</div>
+      <DateTime>{comment.createdAt}</DateTime>
+      <MyLink to={`/post/${comment.Post.id}?comment=${comment.id}`}>
+        #ref
+      </MyLink>
     </div>
+    <hr className="my-2" />
+    <p>
+      {comment.description.substring(0, descriptionLimit) +
+        (comment.description.length > descriptionLimit ? "…" : "")}
+    </p>
+    <StandalonePost post={comment.Post} />
   </Panel>
 );
