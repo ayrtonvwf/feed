17b6f6b62d1555d3d8d03437c0aea7470e67d921
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-11-06 08:53:57 -0300

    feat: adjust standalone-post component styling #23
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

--- app/components/feed/standalone-post.tsx
+++ app/components/feed/standalone-post.tsx
@@ -1,5 +1,6 @@
 import { Feed, Post, User } from "@prisma/client";
 import { Panel } from "../block/panel";
+import { DateTime } from "../typography/date-time";
 import { MyLink } from "../typography/link";
 
 export type StandalonePostType = Post & { User: User; Feed: Feed };
@@ -9,14 +10,24 @@ const descriptionLimit = 100;
 export const StandalonePost: React.FC<{ post: StandalonePostType }> = ({
   post,
 }) => (
-  <Panel key={post.id}>
-    <div className="mb-5 flex flex-col gap-2">
-      <div>
-        <MyLink to={`/user/${post.User.id}`}>{post.User.name}</MyLink>
-        (on <MyLink to={`/feed/${post.Feed.id}`}>{post.Feed.title}</MyLink>)
-        <span>{post.createdAt.toLocaleString()}</span>
-        <MyLink to={`/post/${post.id}`}>Referência</MyLink>
+  <Panel key={post.id} className="border-[1px] border-solid border-gray-300">
+    <div>
+      <div className="flex gap-2">
+        <div>
+          <MyLink to={`/user/${post.User.id}`}>{post.User.name}</MyLink>
+        </div>
+        <div>
+          (on <MyLink to={`/feed/${post.Feed.id}`}>{post.Feed.title}</MyLink>)
+        </div>
+        <div>|</div>
+        <div>
+          <DateTime>{post.createdAt}</DateTime>
+        </div>
+        <div>
+          <MyLink to={`/post/${post.id}`}>referência</MyLink>
+        </div>
       </div>
+      <hr className="my-2" />
       <h3 className="font-bold">{post.title}</h3>
       <p>
         {post.description.substring(0, descriptionLimit) +
