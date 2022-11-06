89458b3524c46f7d07f0e498dbbfb0f113994707
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-11-06 15:24:30 -0300

    feat: create user tenant and automatically authenticate the created user #6
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

--- app/routes/create-account/index.tsx
+++ app/routes/create-account/index.tsx
@@ -10,6 +10,7 @@ import { MyH1 } from "~/components/typography/title";
 import { authenticator } from "~/services/auth.server";
 import { hash } from "~/services/hash.server";
 import { prisma } from "~/services/prisma.server";
+import { commitSession, getSession } from "~/services/session.server";
 import { ulid } from "~/services/uild.server";
 
 export const loader: LoaderFunction = async ({
@@ -47,9 +48,53 @@ export const action: ActionFunction = async ({
       passwordHash: await hash({ password }),
     },
   });
+
+  const tenant = await prisma.tenant.create({
+    data: {
+      id: ulid(),
+      name: `${user.name}'s Tenant`,
+      TenantUser: {
+        create: {
+          id: ulid(),
+          userId: user.id,
+          type: "MANAGER",
+        },
+      },
+      Feed: {
+        create: {
+          id: ulid(),
+          title: `Default Feed`,
+          Post: {
+            create: {
+              id: ulid(),
+              title: `Welcome to ${user.name}'s Tenant`,
+              description: `This is the first post of the whole Tenant. Feel free to explore! (automatically generated)`,
+              userId: user.id,
+            },
+          },
+        },
+      },
+    },
+  });
+
   await prisma.$disconnect();
 
-  return redirect(`/login`);
+  /**
+   * I would rather create a custom auth strategy but it looks like the Strategy
+   * class will only get the request object and not the user entity directly so
+   * I guess we have to bypass it as shown in the docs.
+   *
+   * @see https://github.com/sergiodxa/remix-auth#custom-redirect-url-based-on-the-user
+   */
+  const session = await getSession(request.headers.get("cookie"));
+  session.set("tenantId", tenant.id);
+  session.set(authenticator.sessionKey, user);
+
+  return redirect(`/`, {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
 };
 
 export default function Index() {
