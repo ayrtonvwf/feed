4119e447143f9e4589bdef595d100b44df142ad3
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-11-06 08:53:57 -0300

    feat: adjust standalone-post component styling #23
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

--- app/components/block/panel.tsx
+++ app/components/block/panel.tsx
@@ -2,7 +2,8 @@ export const Panel: React.FC<React.HTMLProps<HTMLDivElement>> = (props) => (
   <div
     {...props}
     className={
-      "my-5 flex flex-col gap-2 bg-white p-5 shadow " + props.className
+      "my-5 flex flex-col gap-2 bg-white p-5 shadow rounded-md " +
+      props.className
     }
   >
     {props.children}
