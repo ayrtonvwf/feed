aab2f1ba41e1e90dfc972ebc275547f14d42b3f5
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-11-08 20:57:36 -0300

    feat: create post detail page #25
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

---
+++ app/routes/post/$postId/load-comments.tsx
@@ -0,0 +1,39 @@
+import { LoaderArgs } from "@remix-run/cloudflare";
+import { typedjson } from "remix-typedjson";
+import invariant from "tiny-invariant";
+import { authenticator } from "~/services/auth.server";
+import { prisma } from "~/services/prisma.server";
+
+export const loader = async ({ request, context, params }: LoaderArgs) => {
+  await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+
+  invariant(
+    typeof params.postId === "string",
+    `params.postId should be a string`
+  );
+
+  const url = new URL(request.url);
+  const after = url.searchParams.get("after")?.toString();
+
+  /**
+   * @see https://www.prisma.io/docs/concepts/components/prisma-client/pagination#cursor-based-pagination
+   */
+  await prisma.$connect();
+  const comments = await prisma.comment.findMany({
+    where: { postId: params.postId },
+    take: 2,
+    skip: 1, // Skip the cursor
+    cursor: {
+      id: after,
+    },
+    include: {
+      User: true,
+    },
+    orderBy: { id: "desc" },
+  });
+  await prisma.$disconnect();
+
+  return typedjson({ comments });
+};
