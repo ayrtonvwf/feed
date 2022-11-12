dd71c848480b3dc3a2fd507c27bc13a5b70e8b4b
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-11-12 20:05:49 -0300

    feat: add load more comments on user profile #19
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

--- app/routes/user/$userId/comments.tsx
+++ app/routes/user/$userId/comments.tsx
@@ -9,6 +9,7 @@ import {
   StandaloneComment,
   StandaloneCommentType,
 } from "~/components/feed/standalone-comment";
+import { MyLink } from "~/components/typography/link";
 import { MyH2 } from "~/components/typography/title";
 import { authenticator } from "~/services/auth.server";
 import { prisma } from "~/services/prisma.server";
@@ -19,7 +20,12 @@ export const loader = async ({
   context,
   params,
 }: LoaderArgs): Promise<
-  TypedJsonResponse<{ comments: StandaloneCommentType[] }>
+  TypedJsonResponse<{
+    comments: StandaloneCommentType[];
+    userId: string;
+    after?: string;
+    prevAfter?: string;
+  }>
 > => {
   await authenticator.isAuthenticated(request, {
     failureRedirect: "/login",
@@ -30,26 +36,53 @@ export const loader = async ({
 
   invariant(params.userId, `params.userId is required`);
 
+  const url = new URL(request.url);
+  const after = url.searchParams.get("after")?.toString();
+  const prevAfter = url.searchParams.get("prevAfter")?.toString();
+
   await prisma.$connect();
   const comments = await prisma.comment.findMany({
     where: { userId: params.userId, Post: { Feed: { tenantId } } },
     include: { Post: { include: { Feed: true, User: true } }, User: true },
+    take: 2,
+    skip: after ? 1 : 0,
+    cursor: after
+      ? {
+          id: after,
+        }
+      : undefined,
+    orderBy: { id: "desc" },
   });
 
   await prisma.$disconnect();
 
-  return typedjson({ comments });
+  return typedjson({ comments, userId: params.userId, after, prevAfter });
 };
 
 export default function () {
-  const { comments } = useTypedLoaderData<typeof loader>();
+  const { comments, userId, after, prevAfter } =
+    useTypedLoaderData<typeof loader>();
+
+  const nextPageUrl = comments.length
+    ? `/user/${userId}/comments?after=${comments[comments.length - 1].id}` +
+      (after ? `&prevAfter=${after}` : "")
+    : null;
+  const prevPageUrl = after
+    ? `/user/${userId}/comments` + (prevAfter ? `?after=${prevAfter}` : "")
+    : null;
 
   return (
     <div>
       <MyH2>Comments</MyH2>
+      {prevPageUrl && <MyLink to={prevPageUrl}>{"<-"} Mais recentes</MyLink>}
       {comments.map((comment) => (
         <StandaloneComment comment={comment} />
       ))}
+      {nextPageUrl ? (
+        <MyLink to={nextPageUrl}>Mais antigos {"->"}</MyLink>
+      ) : (
+        <div>Não há comentários mais antigos</div>
+      )}
     </div>
   );
 }
