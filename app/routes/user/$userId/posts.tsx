dcccce578ab85c53fdb192ad2400f02368c476e7
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2022-11-12 20:09:18 -0300

    feat: add load more posts on user profile #19
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

--- app/routes/user/$userId/posts.tsx
+++ app/routes/user/$userId/posts.tsx
@@ -9,6 +9,7 @@ import {
   StandalonePost,
   StandalonePostType,
 } from "~/components/feed/standalone-post";
+import { MyLink } from "~/components/typography/link";
 import { MyH2 } from "~/components/typography/title";
 import { authenticator } from "~/services/auth.server";
 import { prisma } from "~/services/prisma.server";
@@ -18,7 +19,14 @@ export const loader = async ({
   request,
   context,
   params,
-}: LoaderArgs): Promise<TypedJsonResponse<{ posts: StandalonePostType[] }>> => {
+}: LoaderArgs): Promise<
+  TypedJsonResponse<{
+    posts: StandalonePostType[];
+    userId: string;
+    after?: string;
+    prevAfter?: string;
+  }>
+> => {
   await authenticator.isAuthenticated(request, {
     failureRedirect: "/login",
   });
@@ -28,26 +36,53 @@ export const loader = async ({
 
   invariant(params.userId, `params.userId is required`);
 
+  const url = new URL(request.url);
+  const after = url.searchParams.get("after")?.toString();
+  const prevAfter = url.searchParams.get("prevAfter")?.toString();
+
   await prisma.$connect();
   const posts = await prisma.post.findMany({
     where: { userId: params.userId, Feed: { tenantId } },
     include: { Feed: true, User: true },
+    take: 2,
+    skip: after ? 1 : 0,
+    cursor: after
+      ? {
+          id: after,
+        }
+      : undefined,
+    orderBy: { id: "desc" },
   });
 
   await prisma.$disconnect();
 
-  return typedjson({ posts });
+  return typedjson({ posts, userId: params.userId, after, prevAfter });
 };
 
 export default function () {
-  const { posts } = useTypedLoaderData<typeof loader>();
+  const { posts, userId, after, prevAfter } =
+    useTypedLoaderData<typeof loader>();
+
+  const nextPageUrl = posts.length
+    ? `/user/${userId}/posts?after=${posts[posts.length - 1].id}` +
+      (after ? `&prevAfter=${after}` : "")
+    : null;
+  const prevPageUrl = after
+    ? `/user/${userId}/posts` + (prevAfter ? `?after=${prevAfter}` : "")
+    : null;
 
   return (
     <div>
       <MyH2>Posts</MyH2>
+      {prevPageUrl && <MyLink to={prevPageUrl}>{"<-"} Mais recentes</MyLink>}
       {posts.map((post) => (
         <StandalonePost post={post} />
       ))}
+      {nextPageUrl ? (
+        <MyLink to={nextPageUrl}>Mais antigos {"->"}</MyLink>
+      ) : (
+        <div>Não há posts mais antigos</div>
+      )}
     </div>
   );
 }
